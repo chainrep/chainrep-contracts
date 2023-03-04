5ba07762486244232bc295155ef1f113eafaddf8
sliponit <sliponit@pm.me>
2023-03-05 00:39:33 +0100

    feat: subgraph handle first event ok

---
+++ chain-rep-subgraph/src/chain-rep.ts
@@ -0,0 +1,75 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  ChainRep,
+  ContractReported,
+  CreateCertificate,
+  DomainReported,
+  IssueCertificate,
+  PublishReport,
+  RevokeCertificate,
+  TagReported,
+  TransferCertificateAuthority,
+  UnPublishReport
+} from "../generated/ChainRep/ChainRep"
+import { Certificate, Report } from "../generated/schema"
+
+export function handleContractReported(event: ContractReported): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  const id = event.params.reportId.toString()
+  const contractAddress = event.params.contractAddress
+  let entity = Report.load(id)
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) entity = new Report(id, event.address, '', true) // TODO??
+
+  // Entity fields can be set based on event parameters
+  entity.contractAddress = contractAddress
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.createCertificate(...)
+  // - contract.getCertifiedContractReports(...)
+  // - contract.getReport(...)
+  // - contract.isCertificateAuthority(...)
+  // - contract.isCertified(...)
+  // - contract.isReviewer(...)
+  // - contract.numCertificates(...)
+  // - contract.numReports(...)
+  // - contract.publishReport(...)
+}
+
+export function handleCreateCertificate(event: CreateCertificate): void {}
+
+export function handleDomainReported(event: DomainReported): void {}
+
+export function handleIssueCertificate(event: IssueCertificate): void {}
+
+export function handlePublishReport(event: PublishReport): void {}
+
+export function handleRevokeCertificate(event: RevokeCertificate): void {}
+
+export function handleTagReported(event: TagReported): void {}
+
+export function handleTransferCertificateAuthority(
+  event: TransferCertificateAuthority
+): void {}
+
+export function handleUnPublishReport(event: UnPublishReport): void {}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,49 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 
-const config: HardhatUserConfig = {
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import { task } from "hardhat/config";
+import "@graphprotocol/hardhat-graph";
+
+task("deploy", "Deploys the passed contract")
+  .addParam("contractName", "The name of the contract")
+  .setAction(async (taskArgs, hre) => {
+    const { contractName } = taskArgs;
+
+    await hre.run("compile");
+
+    const address = await deploy(hre, contractName);
+
+    await hre.run("graph", { contractName, address });
+  });
+
+const deploy = async (hre: any, contractName: string): Promise<string> => {
+  const contractArtifacts = await hre.ethers.getContractFactory(contractName);
+  const contract = await contractArtifacts.deploy();
+
+  await contract.deployed();
+
+  return contract.address;
+};
+
+const config = {
   solidity: "0.8.19",
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+    hardhat: {},
+  },
+  subgraph: {
+    name: "chain-rep",
+  },
+  paths: {
+    subgraph: "chain-rep-subgraph",
+  },
 };
 
 export default config;
