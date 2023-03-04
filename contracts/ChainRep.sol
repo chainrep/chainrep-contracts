b354350a9574d08b583f06c6bbd6d6d1f88f83c0
midpoint68 <trmidpoint@gmail.com>
2023-03-03 23:08:10 -0700

    trim interface and implement all functions

--- contracts/ChainRep.sol
+++ contracts/ChainRep.sol
@@ -3,73 +3,113 @@ pragma solidity ^0.8.0;
 
 import { IChainRep } from "./IChainRep.sol";
 import { Context } from "@openzeppelin/contracts/utils/Context.sol";
+import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract ChainRep is IChainRep, Context {
 
-  uint256 private _certificateId;
-  uint256 private _reportId;
+    using EnumerableSet for EnumerableSet.UintSet;
 
-  mapping(uint256 => mapping(address => bool)) private _certificateIssued;
-  mapping(uint256 => Certificate) private _certificateMap;
-  mapping(uint256 => Report) private _reportMap;
-  mapping(address => uint256[]) private _contractReports;
-  mapping(string => uint256[]) private _domainReports;
+    uint256 private _certificateId;
+    uint256 private _reportId;
 
-  constructor() { }
+    mapping(uint256 => mapping(address => bool)) private _certificateIssued;
+    mapping(uint256 => Certificate) private _certificateMap;
+    mapping(uint256 => Report) private _reportMap;
+    mapping(address => EnumerableSet.UintSet) private _contractReports;
 
-  modifier isAuthority(uint256 certificateId) {
-    require(_certificateMap[certificateId].authority == _msgSender(), "not authority");
-    _;
-  }
+    constructor() { }
 
-  function numCertificates () external view returns(uint256) {
-    return _certificateId;
-  }
+    modifier isAuthority(uint256 certificateId) {
+        require(_certificateMap[certificateId].authority == _msgSender(), "not authority");
+        _;
+    }
 
-  function issueCertificate (uint256 certificateId, address reviewer) external isAuthority(certificateId) {
-    require(!_certificateIssued[certificateId][reviewer], "already issued");
-    _certificateIssued[certificateId][reviewer] = true;
-    emit IssueCertificate(certificateId, _msgSender(), reviewer);
-  }
+    function numCertificates () external view returns(uint256) {
+        return _certificateId;
+    }
 
-  function revokeCertificate (uint256 certificateId, address reviewer) external isAuthority(certificateId) {
-    require(_certificateIssued[certificateId][reviewer], "not issued");
-    delete _certificateIssued[certificateId][reviewer];
-    emit RevokeCertificate(certificateId, _msgSender(), reviewer);
-  }
+    function issueCertificate (uint256 certificateId, address reviewer) external isAuthority(certificateId) {
+        require(!_certificateIssued[certificateId][reviewer], "already issued");
+        _certificateIssued[certificateId][reviewer] = true;
+        emit IssueCertificate(certificateId, _msgSender(), reviewer);
+    }
 
-  function createCertificate (string calldata name) external {
-    uint256 id = _certificateId++;
-    _certificateMap[id].name = name;
-    _certificateMap[id].authority = _msgSender();
-    emit CreateCertificate(id, _msgSender(), name);
-  }
+    function revokeCertificate (uint256 certificateId, address reviewer) external isAuthority(certificateId) {
+        require(_certificateIssued[certificateId][reviewer], "not issued");
+        delete _certificateIssued[certificateId][reviewer];
+        emit RevokeCertificate(certificateId, _msgSender(), reviewer);
+    }
 
-  function transferCertificateAuthority (uint256 certificateId, address to) external isAuthority(certificateId) {
-    _certificateMap[certificateId].authority = to;
-    emit TransferCertificateAuthority(certificateId, _msgSender(), to);
-  }
+    function createCertificate (string calldata name) external {
+        uint256 id = _certificateId++;
+        _certificateMap[id].name = name;
+        _certificateMap[id].authority = _msgSender();
+        emit CreateCertificate(id, _msgSender(), name);
+    }
 
-  function numReports () external view returns(uint256) {
-    return _reportId;
-  }
+    function transferCertificateAuthority (uint256 certificateId, address to) external isAuthority(certificateId) {
+        _certificateMap[certificateId].authority = to;
+        emit TransferCertificateAuthority(certificateId, _msgSender(), to);
+    }
 
-  function publishContractReport (address contractAddress, string calldata uri, string[] calldata tags) external {
+    function numReports () external view returns(uint256) {
+        return _reportId;
+    }
 
-  }
+    function publishReport (address[] calldata contractAddresses, string[] calldata domains, string[] calldata tags, string calldata uri) external {
+        uint256 id = _reportId++;
+        _reportMap[id].reviewer = _msgSender();
+        _reportMap[id].contractAddresses = contractAddresses;
+        _reportMap[id].domains = domains;
+        _reportMap[id].tags = tags;
+        _reportMap[id].uri = uri;
+        emit PublishReport(id, _msgSender(), _reportMap[id]);
+    }
 
-  function publishDomainReport (string calldata domain, string calldata uri, string[] calldata tags) external {
+    function unPublishReport (uint256 reportId) external {
+        Report memory report = _reportMap[reportId];
+        for(uint256 i; i < report.contractAddresses.length; i++) {
+            _contractReports[report.contractAddresses[i]].remove(reportId);
+        }
+    }
 
-  }
+    function getReport (uint256 reportId) external view returns(Report memory) {
+        require(reportId < _reportId, "report dne");
+        return _reportMap[reportId];
+    }
 
-  // function getReport (uint256 reportId) external view returns(Report memory)
+    /**
+    * @dev NOTE: O(n*m) time where n is # of reports and m is # of certificates searched
+    */
+    function getCertifiedContractReports (address contractAddress, uint256[] memory certificateIds) external view returns(Report[] memory) {
+        uint256 maxLength = _contractReports[contractAddress].length();
+        uint256 numCertified;
+        Report[] memory res = new Report[](maxLength);
+        for(uint256 i = 0; i < maxLength; i++) {
+            uint256 reportId = _contractReports[contractAddress].at(i);
+            for(uint256 j = 0; j < certificateIds.length; j++) {
+                if(isCertified(_reportMap[reportId].reviewer, certificateIds[j])) {
+                    res[numCertified++] = _reportMap[reportId];
+                    break;
+                }
+            }
+        }
+        if(numCertified < maxLength) {
+            Report[] memory resTrimmed = new Report[](numCertified);
+            for(uint256 i = 0; i < numCertified; i++) {
+                resTrimmed[i] = res[i];
+            }
+            res = resTrimmed;
+        }
+        return res;
+    }
 
-  // function getCertifiedContractReports (address contractAddress, uint256[] memory certifications) external view returns(Report[] memory)
+    function isCertificateAuthority (address authority, uint256 certificateId) public view returns(bool) {
+        return authority == _certificateMap[certificateId].authority;
+    }
 
-  // function getCertifiedDomainReports (string memory domain, uint256[] memory certifications) external view returns(Report[] memory)
-
-  // function isCertificateAuthority (address authority, uint256 certificate) public view returns(bool)
-
-  // function isCertified (address reviewer, uint256 certificate) public view returns(bool)
+    function isCertified (address reviewer, uint256 certificateId) public view returns(bool) {
+        return _certificateIssued[certificateId][reviewer];
+    }
 
 }
--- contracts/IChainRep.sol
+++ contracts/IChainRep.sol
@@ -3,59 +3,55 @@ pragma solidity ^0.8.0;
 
 interface IChainRep {
 
-  struct Report {
-    address reviewer;
-    address[] contractAddresses;
-    string[] domains;
-    string[] tags;
-    string uri;
-  }
+    struct Report {
+        address reviewer;
+        address[] contractAddresses;
+        string[] domains;
+        string[] tags;
+        string uri;
+    }
 
-  struct Certificate {
-    address authority;
-    string name;
-  }
+    struct Certificate {
+        address authority;
+        string name;
+    }
 
-  event IssueCertificate(uint256 indexed certificateId, address indexed authority, address indexed reviewer);
+    event IssueCertificate(uint256 indexed certificateId, address indexed authority, address indexed reviewer);
 
-  event RevokeCertificate(uint256 indexed certificateId, address indexed authority, address indexed reviewer);
+    event RevokeCertificate(uint256 indexed certificateId, address indexed authority, address indexed reviewer);
 
-  event CreateCertificate(uint256 indexed certificateId, address indexed authority, string name);
+    event CreateCertificate(uint256 indexed certificateId, address indexed authority, string name);
 
-  event TransferCertificateAuthority(uint256 indexed certificateId, address indexed from, address indexed to);
+    event TransferCertificateAuthority(uint256 indexed certificateId, address indexed from, address indexed to);
 
-  event PublishReport(uint256 indexed reportId, address indexed reviewer, Report report);
+    event PublishReport(uint256 indexed reportId, address indexed reviewer, Report report);
 
-  event UnPublishReport(uint256 indexed reportId, address indexed reviewer, Report report);
-  
-  function numCertificates () external view returns(uint256);
+    event UnPublishReport(uint256 indexed reportId, address indexed reviewer, Report report);
 
-  function issueCertificate (uint256 certificateId, address reviewer) external;
+    function numCertificates () external view returns(uint256);
 
-  function revokeCertificate (uint256 certificateId, address reviewer) external;
+    function issueCertificate (uint256 certificateId, address reviewer) external;
 
-  function createCertificate (string calldata name) external;
+    function revokeCertificate (uint256 certificateId, address reviewer) external;
 
-  function transferCertificateAuthority (uint256 certificateId, address to) external;
+    function createCertificate (string calldata name) external;
 
-  function numReports () external view returns(uint256);
+    function transferCertificateAuthority (uint256 certificateId, address to) external;
 
-  function publishContractReport (address contractAddress, string calldata uri, string[] calldata tags) external;
+    function numReports () external view returns(uint256);
 
-  function publishDomainReport (string calldata domain, string calldata uri, string[] calldata tags) external;
+    function publishReport (address[] calldata contractAddresses, string[] calldata domains, string[] calldata tags, string calldata uri) external;
 
-  function unPublishReport (uint256 reportId) external;
+    function unPublishReport (uint256 reportId) external;
 
-  function getReport (uint256 reportId) external view returns(Report memory);
+    function getReport (uint256 reportId) external view returns(Report memory);
 
-  function getCertifiedContractReports (address contractAddress, uint256[] memory certifications) external view returns(Report[] memory);
+    function getCertifiedContractReports (address contractAddress, uint256[] memory certificateIds) external view returns(Report[] memory);
 
-  function getCertifiedDomainReports (string memory domain, uint256[] memory certifications) external view returns(Report[] memory);
+    function isCertificateAuthority (address authority, uint256 certificateId) external view returns(bool);
 
-  function isCertificateAuthority (address authority, uint256 certificate) external view returns(bool);
+    function isCertified (address reviewer, uint256 certificateId) external view returns(bool);
 
-  function isCertified (address reviewer, uint256 certificate) external view returns(bool);
-
-  // listCertificates
+    // listCertificates
 }
 
